3ef5107087a5385d3303975f9bcaa48774175c6a
Kenneth Lim <kennethlimcp@gmail.com>
2017-03-23 12:43:32 +0800

    refactor: breakout aesFunction
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- aes-demo.py
+++ aes-demo.py
@@ -4,8 +4,7 @@
 # AES demo code for pycrypto package
 
 import os
-from Crypto.Cipher import AES
-from Padding import *
+from aesFunction import aesFunction
 
 def generateAesKey():
     key = os.urandom(32) # 32 bytes is 32*8 = 256bits
@@ -19,23 +18,6 @@ def generateRandomPlainText():
     pt = os.urandom(31) # 31 bytes is two blocks
     print("Random text byte size: ", len(pt))
     return pt
-
-def aesFunction(key, iv, text, mode):
-    aes = obj = AES.new(key, AES.MODE_CBC, iv)
-    if(mode.lower() == 'e'):
-        padded = text
-        if(len(padded)%16 != 0):
-            padded = pad(padded,16,'pkcs7')
-
-            print("padded:             ", padded)
-        return aes.encrypt(padded)
-    elif(mode.lower() == 'd'):
-        decrypted = aes.decrypt(text)
-        print("decrypted with pad: ", decrypted)
-        unpadded = unpad(decrypted,16,'pkcs7')
-        print("unpadded:           ", unpadded)
-        return unpadded
-
 
 if __name__=="__main__":
     keyFile = open("key", 'wb')
