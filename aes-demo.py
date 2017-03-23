2d07470ae55689d520db408f5d12cf0749680ddf
Kenneth Lim <kennethlimcp@gmail.com>
2017-03-23 12:14:51 +0800

    add: PKCS7 padding function
    - from pycrypto implementation
    - updated aes demo to use odd bytes with pad/unpad
    
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- aes-demo.py
+++ aes-demo.py
@@ -5,6 +5,7 @@
 
 import os
 from Crypto.Cipher import AES
+from Padding import *
 
 def generateAesKey():
     key = os.urandom(32) # 32 bytes is 32*8 = 256bits
@@ -15,15 +16,26 @@ def generateIV():
     return iv
 
 def generateRandomPlainText():
-    pt = os.urandom(32) # 31 bytes is two blocks
+    pt = os.urandom(31) # 31 bytes is two blocks
+    print("Random text byte size: ", len(pt))
     return pt
 
 def aesFunction(key, iv, text, mode):
     aes = obj = AES.new(key, AES.MODE_CBC, iv)
     if(mode.lower() == 'e'):
-        return aes.encrypt(text)
+        padded = text
+        if(len(padded)%16 != 0):
+            padded = pad(padded,16,'pkcs7')
+
+            print("padded:             ", padded)
+        return aes.encrypt(padded)
     elif(mode.lower() == 'd'):
-        return aes.decrypt(text)
+        decrypted = aes.decrypt(text)
+        print("decrypted with pad: ", decrypted)
+        unpadded = unpad(decrypted,16,'pkcs7')
+        print("unpadded:           ", unpadded)
+        return unpadded
+
 
 if __name__=="__main__":
     keyFile = open("key", 'wb')
@@ -41,13 +53,13 @@ if __name__=="__main__":
     plaintextFile.write(plaintext)
     plaintextFile.close()
     print("Plaintext generated!")
-    print("plaintext: ", plaintext)
+    print("plaintext:          ", plaintext)
 
     encrypted = aesFunction(key,iv,plaintext, 'e')
-    print("encypted ", encrypted)
+    print("encypted:           ", encrypted)
 
     decrypted = aesFunction(key,iv,encrypted, 'd')
-    print("decrypted: ", decrypted)
+    print("decrypted:          ", decrypted)
 
     if(plaintext == decrypted):
         print ("plaintext == decrypted")
