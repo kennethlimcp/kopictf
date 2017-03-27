2a42ff42bf27a239c7f9dad8305790aeada475d6
Kenneth Lim <kennethlimcp@gmail.com>
2017-03-27 09:37:21 +0800

    add: hardcoded 2nd last byte attack
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- test-attack.py
+++ test-attack.py
@@ -27,6 +27,27 @@ if __name__=="__main__":
 
     print("\n\nPerforming mutation")
 
+    guessedPlainText = ""
+
+    #2nd last byte
+    for correctByte in range(0, 256):
+        mutatedCipherByte = encrypted[14] ^ correctByte ^ 0x02 #XOR in int
+        mutatedCipherByteStr = mutatedCipherByte.to_bytes(1, 'little') #convert to byte format
+
+        mutatedCipherText = encrypted[0:14] + mutatedCipherByteStr + (encrypted[15]^54^0x02).to_bytes(1, 'little') + encrypted[16:32]
+
+        decrypted = aesCBCgiven(key,iv,mutatedCipherText, 'd')
+
+        if(decrypted != "Padding error bitch!"):
+            print(mutatedCipherText)
+            print("correctByte", correctByte)
+            print("value: " ,chr(correctByte))
+            guessedPlainText = chr(correctByte) + guessedPlainText
+
+    exit()
+
+
+    #last byte
     for correctByte in range(0, 256):
         mutatedCipherByte = encrypted[15] ^ correctByte ^ 0x01 #XOR in int
         mutatedCipherByteStr = mutatedCipherByte.to_bytes(1, 'little') #convert to byte format
@@ -39,3 +60,4 @@ if __name__=="__main__":
             print(mutatedCipherText)
             print("correctByte", correctByte)
             print("value: " ,chr(correctByte))
+            guessedPlainText = chr(correctByte) + guessedPlainText
