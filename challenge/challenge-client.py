b88f0ceec2430146349857b904790328d76f621a
Kenneth Lim <kennethlimcp@gmail.com>
2017-03-30 11:01:56 +0800

    update: ip address in skeleton code
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- challenge/challenge-client.py
+++ challenge/challenge-client.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# Lim Chen Pin Kenneth
-# 28 March 2017
-# Client starter code for challenge
-# Pair programmed with Ryan Lim
+# Team reyonnnnnnnnnnnnnnnnnnnn
+
 
 import socket
 
 if __name__ == "__main__":
-	# NOTE: UPPERCASE names for constants is a (nice) Python convention
-	URL = 'localhost'
+  	URL = "54.254.162.148"
 	PORT = 1337
 
 	encrypted = b';e\xc6\xd3\xb5\xed\xcaz\xd82\x97{`\x02\xd0\xee\xdf%\x18\xeaf\xaa/,\'3\xael\x83\xd9\xf2u\xda\'\xf5\xb0\xad"q\xfa\xf1\n\xecRZ?rh\x92{\x07\xaf@J4Y\xd2\x9a\xad9\xf0\xf4\x90\xf1'
@@ -21,7 +18,6 @@ if __name__ == "__main__":
 	#connect to server
 	conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	conn.connect ((URL,PORT))
-
 
 	#send your ciphertext (as bytes) to the Oracle
 	conn.send(cipherText)
