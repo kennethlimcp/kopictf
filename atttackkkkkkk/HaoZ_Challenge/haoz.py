e1a56e00e55942b189b9b350a0afb564eae4e5f9
Kenneth Lim <kennethlimcp@gmail.com>
2017-04-08 14:47:24 +0800

    improve: haoz.py
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- atttackkkkkkk/HaoZ_Challenge/haoz.py
+++ atttackkkkkkk/HaoZ_Challenge/haoz.py
@@ -1,42 +1,46 @@
 from itertools import product
 import string
+import math
+import hashlib
 
 def blockCipher(input, key):
     print("Decrypting one block")
-    return input + int.from_bytes(key, 'little')
+    #need a way to add byte by byte and concatenate properly
+    return input + key
 
 
 if __name__ == "__main__":
-    print("Solving the Hao's challenge")
 
-    # 128 bits
-    iv = 0x09bf1e72affb6a192448a572e11e4620
+    print("Solving the Hao's challenge")
+    iv = hashlib.md5("HAOZ".encode("utf-8")).hexdigest()
+    iv = int(iv, 16)
+    print("IV ", hex(iv))
+
+    key = hashlib.md5("CTF".encode("utf-8")).hexdigest()
+    key = int(key, 16)
+    print("KEY", hex(key))
+
+    # 128 bits (16 bytes)
+    # iv = 0x09bf1e72affb6a192448a572e11e4620
 
     # Key is 3 character long, uppercase alphabetical
     # 24bits 3 byte long
-    charList = string.ascii_uppercase
-    keywords = [''.join(i) for i in product(charList, repeat = 3)]
+    #charList = string.ascii_uppercase
+    #keywords = [''.join(i) for i in product(charList, repeat = 3)]
 
     # First four bytes 0x25 0x50 0x44 0x46 (%PDF)
 
     inFile = open("secret.pdf", 'rb')
     inputData = inFile.read()
-
     inFile.close()
 
-    for key in keywords:
-        print("key", key)
+    cipherText = int.from_bytes(inputData[0:16], 'little')
+    print("CIP", hex(cipherText))
 
-        decrypted = blockCipher(iv, key)
-        cipherText = int.from_bytes(inputData[0:16], 'little')
+    decrypted = blockCipher(iv, key)
+    plainText = decrypted ^ cipherText
+    print(hex(plainText))
 
-        print("decrypted", len(decrypted))
-        print("cipherText", len(cipherText))
-
-        plainText = decrypted ^ cipherText
-
-        if(plainText == 0x25504446):
-            print("Found match!")
-            exit()
-
+    if(hex(plainText >> 8*12) == 0x25504446):
+        print("Found match!")
         exit()
--- atttackkkkkkk/HaoZ_Challenge/info.md
+++ atttackkkkkkk/HaoZ_Challenge/info.md
@@ -17,6 +17,10 @@ IV: 09bf1e72affb6a192448a572e11e4620
 
 Block size: 128bit (128/8 = 16 bytes)
 
+Input: 0x41 0x42
+Key:   0x01 0x02
+Ouput: 0x42 0x44
+
 
 ### Resources
 - https://en.wikipedia.org/wiki/Block_cipher_mode_of_operation#Cipher_Feedback_.28CFB.29
